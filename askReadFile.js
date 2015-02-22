9e9633109756459772649886b2aaf936ca348f44
Charles Baldo <charlesbaldo@gmail.com>
2015-02-21 22:40:31 -0500

    Added exit on error

--- askReadFile.js
+++ askReadFile.js
@@ -8,7 +8,8 @@ process.stdin.on('data', function (filename) {
   var filePath = path.join(__dirname, filename.toString().replace(/(\n|\r|\r\n)$/, '') );
   var contents = fs.readFile(filePath,'utf8',function(err,contents){
 		if(err){
-			console.log('Error ' + err);
+			console.log(err.errno);
+			process.exit();
 		} 
 		else
 		{
